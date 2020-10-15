5b0e3b4774c7d3b8b6775698359cf7114623baab
Domiciano <domiciano.rincon@correounivalle.edu.co>
2020-10-15 17:16:36 -0500

    Ejercicio en clase

--- app/src/main/java/co/domi/clase10/activities/ChatActivity.java
+++ app/src/main/java/co/domi/clase10/activities/ChatActivity.java
@@ -2,13 +2,24 @@ package co.domi.clase10.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import co.domi.clase10.R;
+import co.domi.clase10.model.Message;
 import co.domi.clase10.model.User;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ChatActivity extends AppCompatActivity {
+import java.util.UUID;
 
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText msgET;
+    private Button sendBtn;
+    private TextView messagesTV;
     private User myUser;
     private User userClicked;
 
@@ -17,9 +28,27 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        msgET = findViewById(R.id.msgET);
+        sendBtn = findViewById(R.id.sendBtn);
+        messagesTV = findViewById(R.id.messagesTV);
+
         myUser = (User) getIntent().getExtras().getSerializable("myUser");
         userClicked = (User) getIntent().getExtras().getSerializable("userClicked");
 
+        sendBtn.setOnClickListener(this);
+
         Toast.makeText(this, "Usted esta en la conversacion de " + myUser.getUsername() + " con " + userClicked.getUsername(), Toast.LENGTH_LONG).show();
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sendBtn:
+                String msg = msgET.getText().toString();
+                Message message = new Message(UUID.randomUUID().toString(), msg, myUser.getId());
+                break;
+        }
     }
 }
