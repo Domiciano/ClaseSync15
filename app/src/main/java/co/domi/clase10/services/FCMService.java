803f7039fc28360485bc0b41b605554922b21ab3
Domiciano <domiciano.rincon@correounivalle.edu.co>
2020-11-01 11:21:09 -0500

    Semana 1

--- app/src/main/java/co/domi/clase10/services/FCMService.java
+++ app/src/main/java/co/domi/clase10/services/FCMService.java
@@ -1,5 +1,6 @@
 package co.domi.clase10.services;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -9,6 +10,8 @@ import com.google.gson.Gson;
 import org.json.JSONObject;
 
 import androidx.annotation.NonNull;
+import co.domi.clase10.activities.ChatActivity;
+import co.domi.clase10.activities.MainActivity;
 import co.domi.clase10.model.FCMMessage;
 import co.domi.clase10.util.NotificationUtil;
 
@@ -27,7 +30,7 @@ public class FCMService extends FirebaseMessagingService {
         Gson gson = new Gson();
         FCMMessage message = gson.fromJson(json, FCMMessage.class);
 
-        NotificationUtil.createNotification(this, "Nuevo mensaje", message.getMessage());
+        NotificationUtil.createNotification(this, "Nuevo mensaje", message.getMessage(), new Intent(this, MainActivity.class));
 
     }
 }
--- app/src/main/java/co/domi/clase10/util/NotificationUtil.java
+++ app/src/main/java/co/domi/clase10/util/NotificationUtil.java
@@ -3,7 +3,9 @@ package co.domi.clase10.util;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -17,7 +19,7 @@ public class NotificationUtil {
 
     private static int idCounter = 0;
 
-    public static void createNotification(Context context, String title, String msg){
+    public static void createNotification(Context context, String title, String msg, Intent intent){
 
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
@@ -25,10 +27,14 @@ public class NotificationUtil {
             manager.createNotificationChannel(channel);
         }
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, idCounter, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         NotificationCompat.Builder builder = new NotificationCompat
                 .Builder(context, CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(msg)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.notificationicon)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH);
